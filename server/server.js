80d0f0881cb66724be3db7d4f93508d620b4320d
sharik <csh23030@glbitm.ac.in>
2026-08-02 13:42:49 +0530

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB();  // then connect db
 const app = express(); // then create server
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174',process.env.CLIENT_URL],
     credentials: true,
 }));
 
